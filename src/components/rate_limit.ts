772fdc89972c23e0a2e870880716e7fe14957595
JLarky <jlarky@gmail.com>
2023-03-24 19:58:50 -0600

    fix typescript errors

--- src/components/rate_limit.ts
+++
@@ -1,8 +0,0 @@
-import { rateLimit, slidingWindow } from './upstash';
-
-const limiter = slidingWindow(2, 5000);
-const limit = (key: string) => rateLimit(limiter, key);
-
-export const checkLimit = () => {
-	return limit('api');
-};
